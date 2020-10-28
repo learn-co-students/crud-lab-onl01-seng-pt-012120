8402e69099d296d5bb7419e13cb5c4d5c5292283
Jmiller1294 <jayman1294@gmail.com>
2020-10-28 06:08:05 -0400

    Done.

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -2,11 +2,33 @@ import React, { Component } from 'react';
 import Reviews from './Reviews';
 
 class ReviewInput extends Component {
+  
+  state = {
+    text: ''
+  }
+
+  handleClick = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.addReview({text: this.state.text, restaurantId: this.props.restaurantId})
+    this.setState({
+      text: ''
+    })
+  }
+
   render() {
     return (
-      <div>
-        Review Input
-      </div>
+      <React.Fragment>
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <input onChange={(event) => this.handleClick(event)} type= 'text' value={this.state.text} name='text'/>
+          <input type= 'submit' value='Submit' />
+        </form>
+      </React.Fragment>
     );
   }
 };
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,9 +3,12 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+    const associatedReviews = this.props.reviews.filter(review => review.restaurantId === this.props.restaurantId);
+    const renderReviews = associatedReviews.map(review => <Review key={review.id} review={review} delete={this.props.deleteReview} />)
+
     return (
       <ul>
-        Reviews
+        {renderReviews}
       </ul>
     );
   }
